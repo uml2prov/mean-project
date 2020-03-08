772b290ff40ac2c2a3d73edc19d81560019b449c
CarlosCSCJ <csaenzadan@jesuitasformacion.com>
2020-03-08 22:30:59 +0100

    estado inicial

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -7,39 +7,5 @@ employeeCtrl.getEmployees = async(req, res, next) => {
     res.json(employees);
 };
 
-employeeCtrl.createEmployee = async (req, res, next) => {
-    const employee = new Employee({
-        name: req.body.name,
-        position: req.body.position,
-        office: req.body.office,
-        salary: req.body.salary
-    });
-    await employee.save();
-    res.status=200;
-    res.json({status: 'Employee created'});
-};
-
-employeeCtrl.getEmployee = async (req, res, next) => {
-    const { id } = req.params;
-    const employee = await Employee.findById(id);
-    res.json(employee);
-};
-
-employeeCtrl.editEmployee = async (req, res, next) => {
-    const { id } = req.params;
-    const employee = {
-        name: req.body.name,
-        position: req.body.position,
-        office: req.body.office,
-        salary: req.body.salary
-    };
-    await Employee.findByIdAndUpdate(id, {$set: employee}, {new: true});
-    res.json({status: 'Employee Updated'});
-};
-
-employeeCtrl.deleteEmployee = async (req, res, next) => {
-    await Employee.findByIdAndRemove(req.params.id);
-    res.json({status: 'Employee Deleted'});
-};
 
 module.exports = employeeCtrl;
